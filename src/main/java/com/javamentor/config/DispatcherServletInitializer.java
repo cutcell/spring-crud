ee9fce4b14c7f7b623a05c2cf04d3780f5e5444d
cutcell <ifield88@gmail.com>
2017-11-29 01:08:17 +0300

    able to see simple welcome page

---
+++ src/main/java/com/javamentor/config/DispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package com.javamentor.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppContextConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
---
+++ src/main/java/com/javamentor/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.javamentor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public ModelAndView welcome(Model model) {
+
+        return new ModelAndView("welcome");
+
+    }
+
+
+}
